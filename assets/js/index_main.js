9260c4867924c4896ad34e5b7774fc4a6fdd6069
John Wu <john.wu@mail.mcgill.ca>
2016-09-20 23:33:43 -0400

    Fixed a few bugs... added stuff to UI

--- assets/js/index_main.js
+++ assets/js/index_main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-	$(document).ready(function(){
-		$("#login").hide();
-		$("#login-choice").click(function(){
-			$("#login").fadeToggle();
-		});
-		$("h3>span").click(function(){
-			$("#login").fadeToggle();
-		});
-	});
+  $(document).ready(function(){
+    $("#login").hide();
+    $("#login-choice").click(function(){
+      $("#login").fadeToggle();
+    });
+    $("h3>span").click(function(){
+      $("#login").fadeToggle();
+    });
+  });
 });
